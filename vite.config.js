57678ca88c34501cfbe15fd8cbd9fcd018fbeb9c
youssef-ahmed-Devlogs <youssefahmedsup@gmail.com>
2023-07-21 23:34:36 +0300

    Setup Vite configration

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
+                'resources/front/css/app.css',
+                'resources/front/js/app.js',
             ],
             refresh: true,
         }),
