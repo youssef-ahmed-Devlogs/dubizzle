06ff62daa19dce4d4f860e59b45022ec0398eb3b
youssef-ahmed-Devlogs <youssefahmedsup@gmail.com>
2023-07-26 21:41:11 +0300

    Create a CanAccessDashboard middleware

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\Dashboard\UserController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'auth.canAccessDashboard'])->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('index');
     Route::get('/settings', [DashboardController::class, 'settings'])->name('settings');
     Route::get('/profile', [DashboardController::class, 'profile'])->name('profile');
